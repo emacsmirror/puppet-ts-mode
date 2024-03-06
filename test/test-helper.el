0e5308c7b0ef44abbfc98856e8710299effc1e3c
Stefan Möding <stm@kill-9.net>
2024-03-06 13:47:45 +0100

    Install gcc to build parser

--- test/test-helper.el
+++ test/test-helper.el
@@ -6,8 +6,15 @@
 
 (message "Running on Emacs %s" emacs-version)
 
+(require 'treesit)
+
 (let* ((cur (if load-in-progress load-file-name buffer-file-name))
        (dir (locate-dominating-file cur "Cask")))
   (load (expand-file-name "puppet-ts-mode" dir)))
+
+(add-to-list 'treesit-language-source-alist
+             '(puppet "https://github.com/tree-sitter-grammars/tree-sitter-puppet"))
+
+(treesit-install-language-grammar 'puppet)
 
 ;;; test-helper.el ends here
