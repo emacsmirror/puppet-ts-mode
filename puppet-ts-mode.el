580276d1e1914407822df0b1746af50f36ff6f6f
Stefan Möding <stm@kill-9.net>
2024-04-30 07:50:28 +0200

    Update keywords header

--- puppet-ts-mode.el
+++ puppet-ts-mode.el
@@ -6,9 +6,9 @@
 ;; Maintainer:       Stefan Möding <stm@kill-9.net>
 ;; Version:          0.1.0
 ;; Created:          <2024-03-02 13:05:03 stm>
-;; Updated:          <2024-04-30 07:47:31 stm>
+;; Updated:          <2024-04-30 07:50:07 stm>
 ;; URL:              https://github.com/smoeding/puppet-ts-mode
-;; Keywords:         puppet, tree-sitter
+;; Keywords:         languages, puppet, tree-sitter
 ;; Package-Requires: ((emacs "29.1"))
 
 ;; This file is NOT part of GNU Emacs.
