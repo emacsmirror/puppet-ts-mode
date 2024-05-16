9608f13126ae9eb68a6a793a8825033a7d7c8c46
Stefan Möding <stm@kill-9.net>
2024-05-16 07:32:39 +0200

    Add syntax unit tests

---
+++ test/syntax-test.el
@@ -0,0 +1,71 @@
+;;; syntax-test.el --- Unit Test Suite  -*- lexical-binding: t; -*-
+
+;; Copyright (c) 2024 Stefan Möding
+
+;; Author: Stefan Möding
+;; Created: <2024-03-02 13:05:03 stm>
+;; Updated: <2024-05-16 07:32:12 stm>
+
+;; This file is NOT part of GNU Emacs.
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; For a full copy of the GNU General Public License
+;; see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Unit test suite for puppet-ts-mode
+
+;; Most test cases are taken from the original puppet-mode.
+
+;;; Code:
+
+(message "Booting Emacs %s and run tests from %s"
+         emacs-version (file-relative-name load-file-name))
+
+
+;;; Requirements
+(require 'ert)
+
+(declare-function puppet-test-with-temp-buffer (content &rest body))
+(declare-function puppet-test-syntax-at (pos))
+
+
+;;; Regular Expressions
+
+(ert-deftest puppet/regular-expression-literal-match-op ()
+  (puppet-test-with-temp-buffer "$foo =~ / class $foo/"
+    (should (eq (puppet-test-syntax-at 9) 'punctuation))
+    (should (eq (puppet-test-syntax-at 21) 'punctuation))))
+
+(ert-deftest puppet/regular-expression-literal-no-match-op ()
+  (puppet-test-with-temp-buffer "$foo !~ / class $foo/"
+    (should (eq (puppet-test-syntax-at 9) 'punctuation))
+    (should (eq (puppet-test-syntax-at 21) 'punctuation))))
+
+(ert-deftest puppet/regular-expression-literal-node ()
+  (puppet-test-with-temp-buffer "node / class $foo/ {}"
+    (should (eq (puppet-test-syntax-at 6) 'punctuation))
+    (should (eq (puppet-test-syntax-at 6) 'punctuation))))
+
+(ert-deftest puppet/regular-expression-literal-selector ()
+  (puppet-test-with-temp-buffer "/ class $foo/=>"
+    (should (eq (puppet-test-syntax-at 1) 'punctuation))
+    (should (eq (puppet-test-syntax-at 13) 'punctuation))))
+
+(ert-deftest puppet/regular-expression-case ()
+  (puppet-test-with-temp-buffer "/ class $foo/:"
+    (should (eq (puppet-test-syntax-at 1) 'punctuation))
+    (should (eq (puppet-test-syntax-at 13) 'punctuation))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; syntax-test.el ends here
